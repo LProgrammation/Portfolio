20f3609c8c5052870908a4556c6802489eb955b5
Louis Potdevin <l.potdevin.informatique@gmail.com>
2024-10-08 19:27:02 +0200

    Initialisation du projet de départ | Model Mvc + Fichier de départ et structure et choix de la chartes graphique

---
+++ public_html/view/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Louis Potdevin</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+
+    <main>
+        <nav>
+
+            <button>
+                <i class="nav-icon fa fa-home"></i>
+                <h2 class="nav-text">Accueil</h2>
+            </button>
+
+            <button>
+                <i class="nav-icon fa fa-briefcase"></i>
+                <h2 class="nav-text">Compétences</h2>
+            </button>
+
+            <button>
+                <i class="nav-icon fa fa-id-card-o"> </i>
+                <h2 class="nav-text">À propos</h2>
+            </button>
+
+            <button>
+                <i class="nav-icon fa fa-book"></i>
+                <h2 class="nav-text">Formations</h2>
+            </button>
+
+        </nav>
+
+        <content>
+            
+            <subContent1>
+            </subContent1>
+
+            <subContent2>
+            </subContent2>
+        </content>
+
+    </main>
+
+    <footer>
+        <h2>Footer</h2>
+    </footer>
+</body>
+
+
+</html>
